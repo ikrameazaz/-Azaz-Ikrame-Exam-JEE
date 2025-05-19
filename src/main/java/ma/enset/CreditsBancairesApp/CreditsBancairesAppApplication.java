5b90289446f3e6f8cc6b4a15275062e90c765e4b
ikrameazaz <ikrameazaz@gmail.com>
2025-05-19 09:32:29 +0100

    commit

--- src/main/java/ma/enset/CreditsBancairesApp/CreditsBancairesAppApplication.java
+++ src/main/java/ma/enset/CreditsBancairesApp/CreditsBancairesAppApplication.java
@@ -1,7 +1,15 @@
 package ma.enset.CreditsBancairesApp;
 
+import ma.enset.CreditsBancairesApp.entities.*;
+import ma.enset.CreditsBancairesApp.repositories.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CreditsBancairesAppApplication {
@@ -9,5 +17,4 @@ public class CreditsBancairesAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CreditsBancairesAppApplication.class, args);
 	}
-
 }
